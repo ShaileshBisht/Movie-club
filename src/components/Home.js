421c672898def823531ecd51ab61e23633027cbf
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-03 19:56:53 +0530

    use axios to fetch top rated movies in Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import Box from "@material-ui/core/Box";
+import API_KEY from "../SecretKey/secretKey";
+import axios from "axios";
 
 function Home() {
+  const [movieData, setMovieData] = useState([]);
+
+  console.log(movieData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(
+          `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en`
+        )
+        .then((data) => setMovieData(data.data.results));
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="home">
-      <h1>i am home</h1>
+      <Box
+        lineHeight={3}
+        textAlign="center"
+        fontSize={30}
+        fontWeight="fontWeightBold"
+        letterSpacing={10}
+        m={1}
+      >
+        Top Rated Movie
+      </Box>
     </div>
   );
 }
