aa425646f30e3a90e9c340a98e85266c6f1212aa
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-05 19:22:04 +0530

    add functon to search bar to prevent refresh

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Search.css";
 import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Search() {
+  const [input, setInput] = useState("");
+
+  const searchMovie = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+
   return (
     <div className="search">
-      <div className="search_input">
+      <form className="search_input" onSubmit={searchMovie}>
         <TextField
           className="search_input1"
           label="Search Movies"
           margin="normal"
           variant="outlined"
+          value={input}
           placeholder="Enter Movie Name"
+          onChange={(e) => setInput(e.target.value)}
         />
-      </div>
+        <IconButton
+          color="primary"
+          aria-label="add to shopping cart"
+          onClick={searchMovie}
+        >
+          <SearchIcon />
+        </IconButton>
+      </form>
+      <h1>{input}</h1>
     </div>
   );
 }
