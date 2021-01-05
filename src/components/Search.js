54f6525f8a9e0df9428027b6f4f28fa6bac2a45c
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-05 19:32:36 +0530

    fetching for search movie using API

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,12 +3,31 @@ import "./Search.css";
 import TextField from "@material-ui/core/TextField";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
+import API_KEY from "../SecretKey/secretKey";
 
 function Search() {
   const [input, setInput] = useState("");
+  const [inputTemp, setInputTemp] = useState("");
+
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${inputTemp}`
+        )
+        .then((data) => setMovieData(data.data.results));
+    };
+    fetchData();
+  }, [inputTemp]);
+
+  console.log(movieData);
 
   const searchMovie = (e) => {
     e.preventDefault();
+    setInputTemp(input);
     setInput("");
   };
 
@@ -32,7 +51,6 @@ function Search() {
           <SearchIcon />
         </IconButton>
       </form>
-      <h1>{input}</h1>
     </div>
   );
 }
