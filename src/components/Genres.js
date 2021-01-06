cf09d8b537ed0aee4dea30de18d451e5d3761314
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-06 12:58:14 +0530

    add buttons to genres

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Genres.css";
+import Button from "@material-ui/core/Button";
 
 function Genres() {
   return (
     <div className="genres">
-      <h1>i am generes</h1>
+      <div className="genres_cate">
+        <Button variant="outlined">Action</Button>
+        <Button variant="outlined" color="primary">
+          Animation
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Comedy
+        </Button>
+        <Button variant="outlined">Fantasy</Button>
+        <Button variant="outlined" color="primary" href="#outlined-buttons">
+          Science Fiction
+        </Button>
+      </div>
     </div>
   );
 }
